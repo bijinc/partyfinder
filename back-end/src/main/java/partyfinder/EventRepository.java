c98e8c91138f982a96e60ebfd3d140bbdfe848ca
Bijin Chakraborty <14847418+BijinC@users.noreply.github.com>
2019-04-22 15:13:31 -0400

    Change repository types to MongoRepository

--- back-end/src/main/java/partyfinder/EventRepository.java
+++ back-end/src/main/java/partyfinder/EventRepository.java
@@ -1,11 +1,13 @@
 package partyfinder;
 
-import org.springframework.data.repository.CrudRepository;
+// import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
 import partyfinder.Event;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface EventRepository extends CrudRepository<Event, Integer> {
+public interface EventRepository extends MongoRepository<Event, Integer> {
 
 }
